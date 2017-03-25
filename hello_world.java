58a0864ea76c66d48054af1283c8c0d6d88e127c
nick3cook <nick3cook@gmail.com>
2017-03-25 13:11:05 -0400

    Update hello_world.java

--- hello_world.java
+++ hello_world.java
@@ -5,6 +5,18 @@
 public class hello_world
 {
     public static void main( String[] args ){
-    System.out.print("Hello !!!!!");
+    System.out.print("Hello !!!!!"); 
+JFrame f=new JFrame();//creating instance of JFrame  
+          
+JButton b=new JButton("click");//creating instance of JButton  
+b.setBounds(100,100,100, 100);//x axis, y axis, width, height  
+          
+f.add(b);//adding button in JFrame  
+          
+f.setSize(500,500);//400 width and 500 height  
+f.setLayout(null);//using no layout managers  
+f.setVisible(true);//making the frame visible  
+  
+  
     }
 }
